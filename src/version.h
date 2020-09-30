876860f93f337ced2ae3ed3e94ff83cd144b0842
TOtto-EclipseIR <58695819+TOtto-EclipseIR@users.noreply.github.com>
2020-09-30 15:40:14 -0700

    Start dev+0005 branch [#5]

--- src/version.h
+++ src/version.h
@@ -6,8 +6,8 @@
 #define VER_MINI  0
 #define VER_MICRO 0
 #define VER_RELEASE 0xA4
-#define VER_BRANCH 0
-#define VER_STRING "v5.09-preAlpha4"
+#define VER_BRANCH 5
+#define VER_STRING "v5.09-Alpha4+0005"
 #define VER_TRUNKNAME "develop"
 #define VER_BRANCHNAME ""
 #define VER_APPVER VER_STRING
