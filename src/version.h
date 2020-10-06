32c3e2f95c96e70f81761a62e1280d714de20759
Tony Otto <58695819+TOtto-EclipseIR@users.noreply.github.com>
2020-10-06 13:54:31 -0700

    Promote v5.09-Alpha4 to current trunk [#5]

--- src/version.h
+++ src/version.h
@@ -6,7 +6,7 @@
 #define VER_RELEASE 0xA4
 #define VER_BRANCH 0
 #define VER_STRING "v5.09-Alpha4"
-#define VER_TRUNKNAME "develop"
+#define VER_TRUNKNAME "current"
 #define VER_MINI  VER_RELEASE
 #define VER_MICRO VER_BRANCH
 #define VER_BRANCHNAME ""
