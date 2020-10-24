eb6072dd0462bca64a3e1f49898dbe4603b83eb8
Tony Otto <58695819+TOtto-EclipseIR@users.noreply.github.com>
2020-10-24 15:44:03 -0700

    Start develop version 5.10B [#5]

--- src/version.h
+++ src/version.h
@@ -3,10 +3,10 @@
 
 #define VER_MAJOR 5
 #define VER_MINOR 10
-#define VER_RELEASE 1
+#define VER_RELEASE 2
 #define VER_BRANCH 0
-#define VER_STRING "v5.10A"
-#define VER_TRUNKNAME "current"
+#define VER_STRING "v5.10B"
+#define VER_TRUNKNAME "develop"
 #define VER_MINI  VER_RELEASE
 #define VER_MICRO VER_BRANCH
 #define VER_BRANCHNAME ""
